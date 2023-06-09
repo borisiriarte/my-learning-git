8a96c50a61b35d67d6f6711965f85a635ea77cb5
Boris2514 <borismiashiro@gmail.com>
2023-06-09 12:04:31 -0400

    Adding extra info inside script file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Learing git");
+
+let array = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let index = 0; index < array.length; index++) {
+  sum = sum + array[index];
+}
+
+console.log(array);
+console.log(sum);
